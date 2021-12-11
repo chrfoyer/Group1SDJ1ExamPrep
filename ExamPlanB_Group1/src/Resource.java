4590ed583a95c5c19a5ddff6fbdb89f21a2900e6
Robert Barta <92293708+robert-barta@users.noreply.github.com>
2021-12-11 14:02:20 +0100

    Resource Class

--- ExamPlanB_Group1/src/Resource.java
+++ ExamPlanB_Group1/src/Resource.java
@@ -1,4 +1,32 @@
 public class Resource
 {
+  private String fileName;
+  private String extension;
 
+  public Resource(String fileName, String extension)
+  {
+    this.fileName = fileName;
+    this.extension = extension;
+  }
+
+  public boolean isPDF()
+  {
+    return extension.equals("pdf");
+  }
+
+  public boolean equals(Object obj)
+  {
+    if (!(obj instanceof Resource))
+    {
+      return false;
+    }
+    Resource other = (Resource) obj;
+    return fileName.equals(other.fileName) && extension.equals(other.extension);
+  }
+
+  public String toString()
+  {
+    return "The file name is " + fileName + ", and the extension is "
+        + extension;
+  }
 }
