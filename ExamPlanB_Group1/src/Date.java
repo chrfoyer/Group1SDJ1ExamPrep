1f1d80823e978bc96009ebde1741ef8ffa289099
Robert Barta <92293708+robert-barta@users.noreply.github.com>
2021-12-11 14:00:39 +0100

    Date methods

--- ExamPlanB_Group1/src/Date.java
+++ ExamPlanB_Group1/src/Date.java
@@ -24,20 +24,43 @@ public class Date {
 
     public boolean isBefore(Date date2)
     {
-        // todo
-        return true; // holder
+        if (this.year < date2.year)
+        {
+            return true;
+        }
+        else if (this.month < date2.month)
+        {
+            return true;
+        }
+        else
+            return this.day < date2.day;
     }
 
-    private boolean isLeapyear()
+    private boolean isLeapYear()
     {
-        // todo
-        return true; // holder
+        return ((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0);
     }
 
     private int daysInMonth()
     {
-        // todo
-        return 42; // holder
+        if ((month == 1) || (month == 3) || (month == 5) || (month
+            == 7) || (month == 8) || (month == 10) || (month
+            == 12))
+        {
+            return 31;
+        }
+        else if ((month == 4) || (month == 6) || (month == 9)
+            || (month == 11)){
+            return 30;
+        }else if (isLeapYear())
+        {
+            return 29;
+        }
+        else
+        {
+            return 28;
+        }
+
     }
 
     private void nextDay()
