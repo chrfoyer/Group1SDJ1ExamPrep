6dbc1ba627b5375667238536ad289d18f1ad6c64
Robert Barta <92293708+robert-barta@users.noreply.github.com>
2021-12-19 20:48:02 +0100

    Date

--- ExamPlanB_Group1/src/Date.java
+++ ExamPlanB_Group1/src/Date.java
@@ -1,103 +1,139 @@
 import java.time.LocalDate;
 
 // TODO: 19/12/2021 NumberofDaysUntil
-public class Date {
-    private int day;
-    private int month;
-    private int year;
+public class Date
+{
+  private int day;
+  private int month;
+  private int year;
 
-    public Date(int day, int month, int year)
+  public Date(int day, int month, int year)
+  {
+    this.day = day;
+    this.month = month;
+    this.year = year;
+  }
+
+  public int getDay()
+  {
+    return day;
+  }
+
+  public int getMonth()
+  {
+    return month;
+  }
+
+  public int getYear()
+  {
+    return year;
+  }
+
+  public boolean isBefore(Date date2)
+  {
+    if (this.year < date2.year)
     {
-        this.day = day;
-        this.month = month;
-        this.year = year;
+      return true;
     }
-
-    public int getDay() {
-        return day;
-    }
-
-    public int getMonth() {
-        return month;
-    }
-
-    public int getYear() {
-        return year;
-    }
-
-    public boolean isBefore(Date date2)
+    else if (this.month < date2.month)
     {
-        if (this.year < date2.year)
-        {
-            return true;
-        }
-        else if (this.month < date2.month)
-        {
-            return true;
-        }
-        else
-            return this.day < date2.day;
+      return true;
     }
+    else
+      return this.day < date2.day;
+  }
 
-    private boolean isLeapYear()
+  private boolean isLeapYear()
+  {
+    return ((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0);
+  }
+
+  private int daysInMonth()
+  {
+    if ((month == 1) || (month == 3) || (month == 5) || (month == 7) || (month
+        == 8) || (month == 10) || (month == 12))
     {
-        return ((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0);
+      return 31;
     }
-
-    private int daysInMonth()
+    else if ((month == 4) || (month == 6) || (month == 9) || (month == 11))
     {
-        if ((month == 1) || (month == 3) || (month == 5) || (month
-            == 7) || (month == 8) || (month == 10) || (month
-            == 12))
-        {
-            return 31;
-        }
-        else if ((month == 4) || (month == 6) || (month == 9)
-            || (month == 11)){
-            return 30;
-        }else if (isLeapYear())
-        {
-            return 29;
-        }
-        else
-        {
-            return 28;
-        }
-
+      return 30;
     }
-
-    private void nextDay()
+    else if (isLeapYear())
     {
-        day++;
-        if (day>daysInMonth()){
-            month++;
-            day=1;
-            if (month==13){
-                year++;
-                month=1;
-            }
-        }
-
+      return 29;
     }
-
-    public int numberOfDaysUntil(Date date2)
+    else
     {
-        // todo
-        return 42; // holder
+      return 28;
     }
 
-    public static Date today()
+  }
+
+  private void nextDay()
+  {
+    day++;
+    if (day > daysInMonth())
     {
-        LocalDate today = LocalDate.now();
-        return new Date(today.getDayOfMonth(),today.getMonthValue(),
-            today().getYear());
+      month++;
+      day = 1;
+      if (month == 13)
+      {
+        year++;
+        month = 1;
+      }
     }
 
+  }
 
+  public int numberOfDaysUntil(Date date2)
+  {
 
-    public Date copy()
+    int date1inDays = year * 365 + day;
+
+    for (int i = 0; i < month - 1; i++)
     {
-
-        return new Date(day,month,year);
+      date1inDays += daysInMonth();
     }
+
+    for (int i = 0; i < year - 1; i++)
+    {
+      if (isLeapYear())
+      {
+        date1inDays++;
+      }
+
+    }
+
+    int date2inDays = date2.getYear() * 365 + date2.getDay();
+
+    for (int i = 0; i < date2.getMonth() - 1; i++)
+    {
+      date2inDays += date2.daysInMonth();
+
+    }
+
+    for (int i = 0; i < date2.getYear() - 1; i++)
+    {
+      if (isLeapYear())
+      {
+        date2inDays++;
+      }
+
+    }
+    return date2inDays - date1inDays;
+  }
+
+  public static Date today()
+  {
+    LocalDate today = LocalDate.now();
+    return new Date(today.getDayOfMonth(), today.getMonthValue(),
+        today().getYear());
+  }
+
+  public Date copy()
+  {
+
+    return new Date(day, month, year);
+  }
 }
