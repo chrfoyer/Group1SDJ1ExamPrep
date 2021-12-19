b1c61441fb7921bf54c73b5b94079340b838e4c8
KamilFischbach <92295061+KamilFischbach@users.noreply.github.com>
2021-12-19 21:02:13 +0100

    Lesson not finished

--- ExamPlanB_Group1/src/Lesson.java
+++ ExamPlanB_Group1/src/Lesson.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Lesson
 {
@@ -11,19 +12,61 @@ public class Lesson
   public Lesson(String topic, Date date, Time start, Time end, Resource[] res)
   {
     //Maybe some exceptions needed.
+    if(topic==null || topic.equals(""))
+    {
+      throw new IllegalArgumentException("Topic cannot be null or empty.");
+    }
+    if(date.isBefore(Date.today()))
+    {
+      throw new IllegalArgumentException("Lesson cannot be in the past.");
+    }
+    if(res==null || res.length==0)
+    {
+      throw new IllegalArgumentException("Resources cannot be null or empty.");
+    }
+    if(start==null || end==null)
+    {
+      throw new IllegalArgumentException("Time cannot be null");
+    }
+
     this.topic=topic;
     this.date=date.copy();
     this.start=start.copy();
     this.end=end.copy();
-    if(res.length>0)
+
+    //resources.addAll(Arrays.asList(res));
+    for (int i = 0; i < res.length; i++)
     {
-      for (int i = 0; i < res.length; i++)
-      {
-        resources.add(res[i]);
-      }
+      resources.add(res[i]);
     }
   }
 
-  
+  public String getTopic()
+  {
+    return topic;
+  }
 
+  public Date getDate()
+  {
+    return date.copy();
+  }
+
+  public Resource[] getResources()
+  {//Casting to Resource[]
+    return (Resource[]) resources.toArray();
+  }
+
+  public ArrayList<Resource> getAllPDFs()
+  {
+    ArrayList<Resource> pdfs = new ArrayList<>();
+    for (int i = 0; i < resources.size(); i++)
+    {
+      if(resources.get(i).isPDF())
+      {
+        pdfs.add(resources.get(i));
+      }
+    }
+    return pdfs;
+  }
+  
 }
