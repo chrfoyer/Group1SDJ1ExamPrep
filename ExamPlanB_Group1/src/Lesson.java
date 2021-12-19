506adda54a45d5e6848b6866545f5d2e57032bfe
KamilFischbach <92295061+KamilFischbach@users.noreply.github.com>
2021-12-19 20:41:40 +0100

    lesson not finished

--- ExamPlanB_Group1/src/Lesson.java
+++ ExamPlanB_Group1/src/Lesson.java
@@ -1,2 +1,29 @@
-public class Lesson {
+import java.util.ArrayList;
+
+public class Lesson
+{
+  private Date date;
+  private ArrayList<Resource> resources;
+  private Time start;
+  private Time end;
+  private String topic;
+
+  public Lesson(String topic, Date date, Time start, Time end, Resource[] res)
+  {
+    //Maybe some exceptions needed.
+    this.topic=topic;
+    this.date=date.copy();
+    this.start=start.copy();
+    this.end=end.copy();
+    if(res.length>0)
+    {
+      for (int i = 0; i < res.length; i++)
+      {
+        resources.add(res[i]);
+      }
+    }
+  }
+
+  
+
 }
