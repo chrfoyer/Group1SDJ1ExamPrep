ed6a646e3d423d411cfd817bd8d48b688ad7059f
chrfoyer <chrfoyer@gmail.com>
2021-12-20 18:41:53 +0100

    Reworked daysUntil
    I reworked daysUntil to take account for leap years and increment the month properly. Also added a test on the bottom that I checked against an online calculator

--- ExamPlanB_Group1/src/Date.java
+++ ExamPlanB_Group1/src/Date.java
@@ -1,6 +1,6 @@
+import java.security.InvalidParameterException;
 import java.time.LocalDate;
 
-// TODO: 19/12/2021 NumberofDaysUntil
 public class Date
 {
   private int day;
@@ -88,52 +88,124 @@ public class Date
 
   public int numberOfDaysUntil(Date date2)
   {
+    // This will be used to find the days in a month, because the days in a month method
+    // Does not have a parameter
+    Date dateHolder;
 
-    int date1inDays = year * 365 + day;
+    // Not true for leap years
+    //int date1inDays = year * 365 + day;
 
-    for (int i = 0; i < month - 1; i++)
+    int date1InDays = 0;
+    // Add days up to year but not including year of date1 (instance)
+    // Starts at year 0 (Merry Christmas and all)
+    for (int i = 0; i < year; i++)
     {
-      date1inDays += daysInMonth();
-    }
-
-    for (int i = 0; i < year - 1; i++)
-    {
-      if (isLeapYear())
+      dateHolder = new Date(1, 1, i);
+      if (dateHolder.isLeapYear())
       {
-        date1inDays++;
+        date1InDays += 366;
+      }
+      else
+      {
+        date1InDays += 365;
       }
 
     }
 
-    int date2inDays = date2.getYear() * 365 + date2.getDay();
-
-    for (int i = 0; i < date2.getMonth() - 1; i++)
+    // Add days up to month not including instance month
+    // Starts in January (1)
+    for (int i = 1; i < month; i++)
     {
-      date2inDays += date2.daysInMonth();
-
+      dateHolder = new Date(1, i, year);
+      date1InDays += dateHolder.daysInMonth();
     }
 
-    for (int i = 0; i < date2.getYear() - 1; i++)
+    // Add day of the month to total days
+    date1InDays += day;
+
+    // Do the same as above but for date 2
+    // Because we are working in the same class, we can pull the instance variables directly (not use getters)
+    // Just like how we do in some equals methods
+    Date date2Holder;
+
+    int date2InDays = 0;
+
+    for (int i = 0; i < date2.year; i++)
     {
-      if (isLeapYear())
+      date2Holder = new Date(1, 1, i);
+      if (date2Holder.isLeapYear())
       {
-        date2inDays++;
+        date2InDays += 366;
       }
-
+      else
+      {
+        date2InDays += 365;
+      }
     }
-    return date2inDays - date1inDays;
+
+    for (int i = 1; i < date2.month; i++)
+    {
+      date2Holder = new Date(1, i, date2.year);
+      date2InDays += date2Holder.daysInMonth();
+    }
+
+    date2InDays += date2.day;
+
+    // Throw an exception is the date in the parameter is before the date in the instance
+    if (date2InDays - date1InDays < 0)
+    {
+      throw new InvalidParameterException(
+          "The date entered in the parameter is before the date in the object");
+    }
+    else
+    {
+      return date2InDays - date1InDays;
+    }
   }
 
   public static Date today()
   {
     LocalDate today = LocalDate.now();
     return new Date(today.getDayOfMonth(), today.getMonthValue(),
-        today().getYear());
+        today.getYear());
+
   }
 
   public Date copy()
   {
 
     return new Date(day, month, year);
+  }
+
+  public boolean equals(Object obj)
+  {
+    if (!(obj instanceof Date))
+    {
+      return false;
+    }
+
+    Date other = (Date) obj;
+    return year == other.year && month == other.month && day == other.day;
+  }
+
+  public String toString()
+  {
+    return day + "/" + month + "/" + year;
+  }
+
+  // Short test of methods
+  public static void main(String[] args)
+  {
+    Date beastSpawn = new Date(3, 3, 1997);
+    System.out.println("The spawn of the Beast From the East:");
+    System.out.println(beastSpawn);
+    System.out.println("Today:");
+    System.out.println(Date.today());
+    System.out.println("Age of the Beast From the East in days");
+    System.out.println(beastSpawn.numberOfDaysUntil(Date.today()));
+    // This will only return true on the day of writing
+    Date nowish = new Date (20, 12, 2021);
+    System.out.println("Are " + nowish + " and today the same?");
+    System.out.println(nowish.equals(Date.today()));
   }
 }
