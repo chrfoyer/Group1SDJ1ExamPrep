aacb125862c4cf6cf92f8c28b08d93e723ee2694
ninawrona <90191093+ninawrona@users.noreply.github.com>
2021-12-20 02:01:43 +0100

    lesson, online lesson and subclasses made

--- ExamPlanB_Group1/src/Lesson.java
+++ ExamPlanB_Group1/src/Lesson.java
@@ -24,6 +24,73 @@ public class Lesson
     }
   }
 
-  
+  public String getTopic(){
+    return topic;
+  }
 
+  public Date getDate(){
+    return date.copy();
+  }
+
+  //returning all variables from arrayList in array
+  public Resource[] getResources(){
+    Resource[] resourcesArray = new Resource[resources.size()];
+    for(int i = 0; i<resources.size();i++){
+      resourcesArray[i] = resources.get(i);
+    }
+    return resourcesArray;
+  }
+
+  //creates a new array and loops through all resources
+  //returns only those resources which are PDFs
+  public ArrayList<Resource> getAllPDFs(){
+    ArrayList<Resource> allPDFs = new ArrayList<>();
+
+    for(int i = 0; i < resources.size(); i++){
+      if(resources.get(i).isPDF()){
+        allPDFs.add(resources.get(i));
+      }
+    }
+
+    return allPDFs;
+  }
+
+  //this method converts end and start time of the lesson to seconds
+  //then we store this variable ind "subtraction" and we use it to create a new Time object, which is returned
+  public Time getDuration(){
+    int subtraction = end.convertToSeconds() - start.convertToSeconds();
+    Time time = new Time(subtraction);
+    return time;
+  }
+
+  //this method checks if startTime is after 8.20 and before endTime
+  //and if endTime is before 21.10
+  public boolean hasValidTime(Time startTime, Time endTime){
+    if(startTime.isBefore(new Time(8, 20, 00)) || endTime.isBefore(startTime)){
+      return false;
+    }
+    if(!(endTime.isBefore(new Time(21, 10, 00)))){
+      return false;
+    }
+    return true;
+  }
+
+  public String getDateTimeString(){
+    return date.getDay() + "/" + date.getMonth() + "/" + date.getYear() + " " + start.copy() + "-" + end.copy();
+  }
+
+  public boolean equals(Object obj){
+    if(!(obj instanceof Lesson)){
+      return false;
+    }
+
+    Lesson other = (Lesson)obj;
+    return other.getTopic().equals(topic) && other.getResources().equals(resources) &&
+            other.getAllPDFs().equals(getAllPDFs())
+            && other.getDateTimeString().equals(getDateTimeString());
+  }
+
+  public String toString(){
+    return "Lesson: " + getTopic() + "\n" + getDateTimeString() + "\n";
+  }
 }
--- ExamPlanB_Group1/src/OnlineLesson.java
+++ ExamPlanB_Group1/src/OnlineLesson.java
@@ -1,2 +1,32 @@
-public class OnlineLesson {
+public abstract class  OnlineLesson extends Lesson{
+
+    private boolean camera;
+
+
+    public OnlineLesson(String topic, Date date, Time start, Time end, Resource[] res, boolean camera){
+        super(topic, date, start, end, res);
+        this.camera = camera;
+    }
+
+    public boolean isCameraRequired(){
+        return camera;
+    }
+
+    public abstract String nameOfSoftware();
+
+    //comparing object of OnlineLesson using superClass
+    //not sure if it's correct
+    public boolean equals(Object obj){
+        if(!(obj instanceof OnlineLesson)){
+            return false;
+        }
+
+        OnlineLesson other = (OnlineLesson) obj;
+        return super.equals(other) && other.isCameraRequired() == isCameraRequired();
+    }
+
+    public String toString(){
+        return super.toString() + "\n" + isCameraRequired();
+    }
+
 }
