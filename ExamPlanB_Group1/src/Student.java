64c339ac962e517d3a39d2fe3cb83dd3a9772be0
KamilFischbach <92295061+KamilFischbach@users.noreply.github.com>
2021-12-21 15:28:56 +0100

    student not finished

--- ExamPlanB_Group1/src/Student.java
+++ ExamPlanB_Group1/src/Student.java
@@ -1,3 +1,40 @@
-public class Student {
+import java.util.ArrayList;
 
+public class Student
+{
+private int studentNumber;
+  private String name;
+  private ArrayList<Grade> grades;
+  private ArrayList<Course> courses;
+  private CoronaPassport coronaPassport;
+
+  public Student(int studentNumber, String name;)
+  {
+    this.name=name;
+    this.studentNumber=studentNumber;
+    this.grades = new ArrayList<>();
+    this.courses = new ArrayList<>();
+    this.coronaPassport = null;
+  }
+
+  public int getStudentNumber()
+  {
+    return studentNumber;
+  }
+
+  public String getName()
+  {
+    return name;
+  }
+
+  public void addGrade(Course course)
+  {
+    for(int i = 0; i<courses.size(); i++)
+    {
+      if(courses.get(i).equals(course))
+      {
+        grades
+      }
+    }
+  }
 }
