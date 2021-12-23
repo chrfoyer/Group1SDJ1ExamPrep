1cfecf48eb09bf02d756177e7cc0c88701eebbce
KamilFischbach <92295061+KamilFischbach@users.noreply.github.com>
2021-12-23 12:15:27 +0100

    Student done

--- ExamPlanB_Group1/src/Student.java
+++ ExamPlanB_Group1/src/Student.java
@@ -2,16 +2,26 @@ import java.util.ArrayList;
 
 public class Student
 {
-private int studentNumber;
+  private int studentNumber;
   private String name;
   private ArrayList<Grade> grades;
   private ArrayList<Course> courses;
   private CoronaPassport coronaPassport;
 
-  public Student(int studentNumber, String name;)
+  public Student(int studentNumber, String name)
   {
-    this.name=name;
-    this.studentNumber=studentNumber;
+    if (studentNumber < 0)//Just to write some exception.
+    {
+      throw new IllegalArgumentException(
+          "The student number cannot be a negative number.");
+    }
+    if (name == null || name.equals(""))
+    {
+      throw new IllegalArgumentException(
+          "Name cannot be neither null or empty String object.");
+    }
+    this.name = name;
+    this.studentNumber = studentNumber;
     this.grades = new ArrayList<>();
     this.courses = new ArrayList<>();
     this.coronaPassport = null;
@@ -27,14 +37,111 @@ private int studentNumber;
     return name;
   }
 
-  public void addGrade(Course course)
+  public void addCourse(Course course)
   {
-    for(int i = 0; i<courses.size(); i++)
+    if (course == null)
     {
-      if(courses.get(i).equals(course))
+      throw new NullPointerException("The course to be added cannot be null.");
+    }
+    courses.add(course);
+  }
+
+  public void addGrade(Grade grade)
+  {
+    for (int i = 0; i < courses.size(); i++)
+    {
+      if (courses.get(i).equals(grade.getCourse()))
       {
-        grades
+        grades.add(grade);
       }
     }
   }
+
+  public void addCoronaPassport(CoronaPassport passport)
+  {
+    //if(hasValidCoronaPassport)
+    if (passport == null)
+    {
+      throw new NullPointerException("Corona passport cannot be null.");
+    }
+    this.coronaPassport = passport;
+    if (!hasValidCoronaPassport())
+    {
+      removeCoronaPassport();
+      throw new IllegalStateException(
+          "The given corona passport is not legal!");
+    }
+  }
+
+  public void removeCoronaPassport()
+  {
+    if (coronaPassport != null)
+    {
+      this.coronaPassport = null;
+    }
+    else
+    {
+      throw new IllegalCallerException("There is no corona passport.");
+    }
+  }
+
+  public boolean hasValidCoronaPassport()
+  {
+    if (!(Date.today().isBefore(coronaPassport.getDate())))
+    {
+      if (coronaPassport.getReason().equals("Vaccinated"))
+      {
+        return true;
+      }
+      else if (coronaPassport.getReason().equals("Tested")
+          && coronaPassport.getDate().numberOfDaysUntil(Date.today()) <= 3)
+      {
+        return true;
+      }
+      else if (coronaPassport.getReason().equals("Infected")
+          && coronaPassport.getDate().numberOfDaysUntil(Date.today()) > 14
+          && coronaPassport.getDate().numberOfDaysUntil(Date.today()) < 180)
+      {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  public double getGradeAverage()
+  {
+    double numberOfGrades = grades.size();
+    double sumOfGrades = 0;
+    for (int i = 0; i < numberOfGrades; i++)
+    {
+      sumOfGrades += grades.get(i).getGrade();
+    }
+    return (sumOfGrades) / numberOfGrades;
+  }
+
+  public Grade[] getAllGrades()
+  {
+    Grade[] allGrades = new Grade[grades.size()];
+
+    for (int i = 0; i < grades.size(); i++)
+    {
+      allGrades[i]=grades.get(i);
+    }
+    return allGrades;
+  }
+
+  public Course[] getAllCourses()
+  {
+    Course[] allCourses = new Course[courses.size()];
+
+    for (int i = 0; i < courses.size(); i++)
+    {
+      allCourses[i]=courses.get(i);
+    }
+    return allCourses;
+  }
+
+
 }
+
+
